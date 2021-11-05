079eff64e879ca482cf4faac183a1ef1550161f5
Michael Sweet <msweet37@gmail.com>
2021-11-05 10:19:53 -0700

    initial commit with wwise and unity assets

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void StartGame()
+    {
+        //SceneManager.UnloadSceneAsync("GamePlay");
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void HomeScene()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+
+}
