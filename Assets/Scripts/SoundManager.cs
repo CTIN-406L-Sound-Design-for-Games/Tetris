8ef1dd6f07236ec42950a564d1fc446749751798
Michael Sweet <msweet37@gmail.com>
2021-11-05 15:32:00 -0700

    wwise hooks

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,51 +6,51 @@ public class SoundManager : MonoBehaviour
 {
     void Update()
     {
-        
+
     }
 
     public void PlayRight()
     {
-        
+        AkSoundEngine.PostEvent("shape_right", GameObject.Find("WwiseGlobal"));
     }
 
     public void PlayLeft()
     {
-        
+        AkSoundEngine.PostEvent("shape_left", GameObject.Find("WwiseGlobal"));
     }
 
     public void PlayFlipUp()
     {
-        
+        AkSoundEngine.PostEvent("shape_flip", GameObject.Find("WwiseGlobal"));
     }
-    
+
     public void PlayFlipDown()
     {
-        
+        AkSoundEngine.PostEvent("shape_flip", GameObject.Find("WwiseGlobal"));
     }
 
     public void PlayDrop()
     {
-        
+        AkSoundEngine.PostEvent("shape_land", GameObject.Find("WwiseGlobal"));
     }
 
     public void PlayLineClear()
     {
-        
+        AkSoundEngine.PostEvent("clear_row", GameObject.Find("WwiseGlobal"));
     }
 
     public void PlayStage1()
     {
-        
+        AkSoundEngine.PostEvent("stage_1", GameObject.Find("WwiseGlobal"));
     }
 
     public void PlayStage2()
     {
-        
+        AkSoundEngine.PostEvent("stage_2", GameObject.Find("WwiseGlobal"));
     }
 
     public void PlayStage3()
     {
-        
+        AkSoundEngine.PostEvent("stage_3", GameObject.Find("WwiseGlobal"));
     }
 }
---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CLEAR_ROW = 1870373793U;
+        static const AkUniqueID GAME_LOSE = 3425053597U;
+        static const AkUniqueID GAME_WIN = 3218375656U;
+        static const AkUniqueID SHAPE_FLIP = 3205476178U;
+        static const AkUniqueID SHAPE_LAND = 597916644U;
+        static const AkUniqueID SHAPE_LEFT = 1269712800U;
+        static const AkUniqueID SHAPE_RIGHT = 3911910199U;
+        static const AkUniqueID STAGE_1 = 4175426381U;
+        static const AkUniqueID STAGE_2 = 4175426382U;
+        static const AkUniqueID STAGE_3 = 4175426383U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID HIEGHT = 522323832U;
+        static const AkUniqueID SCORE = 2398231425U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TETRIS_SOUNDBANK = 3052682920U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
